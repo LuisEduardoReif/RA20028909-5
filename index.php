3d262b764527e2ea1397b8e83bc8a8e6429e1ac0
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-07 20:49:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,20 @@
 <body>
     <main> 
         <header>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
             <h1>O Bootstrap é bem legal</h1>
         </header>
 
