2e2031a6aa95c5b208d27d1c11e8c64cd36c0402
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-07 20:44:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
     <title>Exemplo Bootstrap</title>
-
 </head>
 <body>
-    <h1>O Bootstrap é bem legal</h1>
+    <main> 
+        <header>
+            <h1>O Bootstrap é bem legal</h1>
+        </header>
+
+   </main>
+    
 </body>
 </html>
