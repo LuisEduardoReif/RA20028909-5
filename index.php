8b137dd403e9ba83e517a03a938fd11cf946ce33
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-07 22:41:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
   </head>
 
   <body class="text-center container d-flex align-items-center justify-content-center" style="height: 100vh;">
-    <form class="form-signin" style="width: 30%">
+    <form class="form-signin" style="width: 30%" method="post" action="./pages/valida.php">
       <h1 class="h3 mb-3 font-weight-normal">Login</h1>
-      <label for="inputEmail" class="sr-only">Email</label>
-      <input type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+      <label for="inputEmail" class="sr-only">R.A.></label>
+      <input type="nunber" name="inputEmail" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
       <label for="inputPassword" class="sr-only">Senha</label>
       <input type="password" name="inputPassword" id="inputPassword" class="mt-3 form-control" placeholder="Senha" required>
       <button class="mt-4 btn btn-lg btn-primary btn-block" type="submit">Login</button>
