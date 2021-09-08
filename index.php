6270f94f168e11b5cee95118f47384c241d351e1
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-07 21:12:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,22 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
-    <title>Exemplo Bootstrap</title>
-</head>
-<body>
-    <main> 
-        <header>
-            <ul class="nav nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-            <h1>O Bootstrap é bem legal</h1>
-        </header>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-   </main>
-    
-</body>
+    <title>Sistema escolar</title>
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center container d-flex align-items-center justify-content-center" style="height: 100vh;">
+    <form class="form-signin" style="width: 30%">
+      <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" name="inputPassword" id="inputPassword" class="mt-3 form-control" placeholder="Senha" required>
+      <button class="mt-4 btn btn-lg btn-primary btn-block" type="submit">Login</button>
+    </form>
+  </body>
 </html>
