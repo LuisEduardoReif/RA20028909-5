cfd4017c09a7781446e5cb65c7b6108850c46dac
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-13 22:24:42 -0300

    Update header.php

--- view/header.php
+++ view/header.php
@@ -9,7 +9,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-
   <title>Sistema escolar</title>
 </head>
 
