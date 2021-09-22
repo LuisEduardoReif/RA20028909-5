fa2c048d042c31e7ec2b5aa3e5a6a365b055c578
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-22 20:21:52 -0300

    Update cursos.php

--- view/cursos.php
+++ view/cursos.php
@@ -1,4 +1,37 @@
 
+<?php 
+
+include_once './header.php';
+include_once '../config.php';
+
+include_once '../models/Courses.php';
+
+$sql = $pdo->query("SELECT * FROM courses");
+
+$array = [];
+
+if($sql->rowCount() > 0){
+
+  $data = $sql->fetchAll();
+
+  foreach($data as $row) {
+    $courses = new Courses();
+    $courses->setId($row['id']);
+    $courses->setNameCourse($row['nameCourse']);
+    $courses->setDescription($row['description']);
+    $courses->setDateStart($row['dateStart']);
+    $courses->setDateFinish($row['dateFinish']);
+    $courses->setStatus($row['status']);
+    $courses->setCreatedAt($row['created_at']);
+    $courses->setUpdatedAt($row['updated_at']);
+
+    $array[] = $courses;
+  }
+
+}
+
+?>
+
   <?php include_once './header.php'?>
 
   <main class="d-flex container align-items-center" style="height: calc(100vh - 56px); ">
@@ -14,19 +47,22 @@
             <tr>
               <th scope="col">ID</th>
               <th scope="col">Nome</th>
-              <th scope="col">Ações</th>
+              <th scope="col">Status</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row" class="align-middle">1</th>
-              <td class="align-middle">Sistemas para Internet</td>
-              <td style="width: 25%">
-                <a type="button" class="btn btn-primary" href="./visualizar_curso.php">Visualizar</a>
-                <a type="button" class="btn btn-secondary" href="./editar_curso.php">Editar</a>
-                <a type="button" class="btn btn-danger" >Excluir</a>
-              </td>
-            </tr>
+            <?php foreach($array as $courses): ?>
+              <tr>
+                <th scope="row" class="align-middle"><?= $courses->getId();?></th>
+                <td class="align-middle"><?= $courses->getNameCourse();?></td>
+                <td class="align-middle"><?php if($courses->getStatus() == 1) {echo 'Ativo';} else {echo 'Inativo';}?></td>
+                <td style="width: 25%">
+                  <a type="button" class="btn btn-primary" href="./visualizar_curso.php">Visualizar</a>
+                  <a type="button" class="btn btn-secondary" href="./editar_curso.php">Editar</a>
+                  <a type="button" class="btn btn-danger" >Excluir</a>
+                </td>
+              </tr>
+            <?php endforeach; ?>
           </tbody>
         </table>
       </div>
