fbc3288afd7bac9975c55f69c3c54952cda0ab2d
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-25 19:00:13 -0300

    Update adicionar_aluno.php

--- view/adicionar_aluno.php
+++ view/adicionar_aluno.php
@@ -28,36 +28,8 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
+  <?php include_once './header.php'?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-
-  <title>Sistema escolar | Cursos</title>
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="./alunos.php">Alunos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./cursos.php">Cursos</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
 
   <main class="d-flex container align-items-center" style="height: calc(100vh - 56px); ">
     <div class="container-fluid border rounded p-5" style="box-shadow: 1px 2px 6px 4px rgba(0,0,0,0.1)">
