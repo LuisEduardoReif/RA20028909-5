372cc48a291bec182dba8c8b09ef6d94f3c19753
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-25 19:00:57 -0300

    Update visualizar_aluno.php

--- view/visualizar_aluno.php
+++ view/visualizar_aluno.php
@@ -1,6 +1,7 @@
 <?php
   include_once '../config.php';
   include_once '../models/Student.php';
+  include_once '../models/Courses.php';
 
   $id = $_GET['id'];
 
@@ -26,7 +27,7 @@
 
   }
 
-  $sql2 = $pdo->query("SELECT * FROM courses WHERE id = $student[0]->getCourse().toString()");
+  $sql2 = $pdo->query("SELECT * FROM courses WHERE id=".$student[0]->getCourse());
 
   $array2 = [];
 
@@ -46,41 +47,10 @@
     $array2[] = $courses;
   }
 
-  var_dump($array2)
-  
-
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
+  <?php include_once './header.php'?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-
-  <title>Sistema escolar | Cursos</title>
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="./alunos.php">Alunos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./cursos.php">Cursos</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
 
   <main class="d-flex container align-items-center" style="height: calc(100vh - 56px); ">
     <div class="container-fluid border rounded p-5" style="box-shadow: 1px 2px 6px 4px rgba(0,0,0,0.1)">
@@ -105,7 +75,7 @@
           <p><?=$student[0]->getName();?></p>
         </div>
         <div class="col-md-3">
-          <p>Ativo</p>
+          <p><?php if($student[0]->getStatus() == 1) {echo 'Ativo';} else {echo'Inativo';}?></p>
         </div>
       </div>
       <div class="row mt-4">
@@ -124,7 +94,7 @@
           <p><?=$student[0]->getPhone();?></p>
         </div>
         <div class="col-md-6">
-          <p><?=$array2[0]->getNameCourse();?></p>
+          <p><?php if($array2) {echo $array2[0]->getNameCourse();} else {echo "Não possui curso";};?></p>
         </div>
         <div class="col-md-3">
           <p><?=$student[0]->getCreatedAt();?></p>
