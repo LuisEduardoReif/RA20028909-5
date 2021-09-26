b6ca83a55753bdfa6496afd274fdb5ec47d8bc1a
LuisEduardoReif <chacaldufloripa@gmail.com>
2021-09-25 21:38:36 -0300

    Update adicionar_aluno.php

--- view/adicionar_aluno.php
+++ view/adicionar_aluno.php
@@ -34,22 +34,21 @@
   <main class="d-flex container align-items-center" style="height: calc(100vh - 56px); ">
     <div class="container-fluid border rounded p-5" style="box-shadow: 1px 2px 6px 4px rgba(0,0,0,0.1)">
       <a type="button" href="./alunos.php" class="btn btn-primary">Voltar</a>
-
       <form action="../controllers/adicionar_aluno_action.php" method="POST" class="mt-5" >
         <div class="row mt-4">
-          <div class="col-md-7">
-            <p class="fw-bold">Nome:</p>
-          </div>
-          <div class="col-md-3">
-            <p class="fw-bold">Status:</p>
-          </div>
+            <div class="col-md-7">
+              <p class="fw-bold">Nome:</p>
+            </div>
+            <div class="col-md-3">
+              <p class="fw-bold">Status:</p>
+            </div>
         </div>
         <div class="row">
           <div class="col-md-7"><input type="text" class="form-control" name="name" id="name" aria-describedby="name"></div>
-          <div class="col-md-5">
-            <select class="form-control" aria-label="Selecione" name="status">
-              <option value="1">Ativo</option>
-              <option value="0">Inativo</option>
+            <div class="col-md-5">
+                <select class="form-control" aria-label="Selecione" name="status">
+                <option value="1">Ativo</option>
+                <option value="0">Inativo</option>
             </select>
           </div>
         </div>
@@ -87,7 +86,7 @@
 
         <button type="submit" class="btn btn-success mr-2">Salvar</button>
       </form>
-    </div>
+
   </main>
 
 </body>
